c9b3beda1f5d36eb5e457f0610f18e9b5dc70d8e
yasithfernando <iyfernando42@gmail.com>
2023-03-30 21:13:23 +0530

    Updated auth feature

--- features/Auth.js
+++ features/Auth.js
@@ -3,26 +3,71 @@ const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
+//Get bcryptjs by 'npm i bcryptjs'
+const bcrypt = require('bcryptjs');
+
 
 
 //REGISTER
 router.post("/register", async (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    /*password: CryptoJS.AES.encrypt(
-      JSON.stringify(req.body.password),
-      process.env.PASS_SEC
-    ).toString(),*/
-  });
+  const {username,email,password} = req.body
 
-  try {
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
-  } catch (err) {
-    res.status(500).json(err);
+  if(!username || !email || !password){
+    res.status(400).json({
+      message: 'Please add all fields'
+    })
   }
+
+  //Check if user exists
+  const userExists = await User.findOne({email})
+
+  if(userExists){
+      res.status(400).json({
+        message: 'User Already Exists'
+      })
+  }
+
+  //Hash the password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  //Create user
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword
+  })
+
+  if(user){
+    res.status(201).json({
+        _id : user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+    })
+  } else{
+    res.status(400).json({
+      message: 'Invalid user datta'
+    })
+  }
+
+
+  // const newUser = new User({
+  //   username: req.body.username,
+  //   email: req.body.email,
+  //   password: req.body.password,
+  //   /*password: CryptoJS.AES.encrypt(
+  //     JSON.stringify(req.body.password),
+  //     process.env.PASS_SEC
+  //   ).toString(),*/
+  // });
+
+  // try {
+  //   const savedUser = await newUser.save();
+  //   res.status(201).json(savedUser);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
 
 
   
@@ -32,37 +77,74 @@ router.post("/register", async (req, res) => {
 
 
 router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({
-      userName: req.body.user_name,
-    });
 
-    if (!user) {
-      return res.status(401).json("Wrong User Name");
+    const {username, password} = req.body
+
+    if(!username || !password){
+        res.status(400).json({
+          message : 'Please enter username and password'
+        })
     }
 
-    const inputPassword = req.body.password;
-    const originalPassword = user.password;
+    //Check user email
+    const user = await User.findOne({username})
 
-    if (originalPassword !== inputPassword) {
-      return res.status(401).json("Wrong Password");
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.json({
+            _id : user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id, user.isAdmin),
+            message : 'Login Succesful!'
+        })
+
+    } else{
+        res.status(400)
+        throw new Error('Invalid Credentials')
     }
 
-    const accessToken = jwt.sign(
-      {
-        id: user._id,
-        isAdmin: user.isAdmin,
-      },
-      process.env.JWT_SEC,
-      { expiresIn: "3d" }
-    );
 
-    const { password, ...others } = user._doc;
-    return res.status(200).json({ ...others, accessToken });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
+  // const {username, password} = req.body
+  // try {
+  //   const user = await User.findOne({username});
+
+  //   if (!user) {
+  //     return res.status(401).json("Wrong User Name");
+  //   }
+
+  //   console.log(password)
+  //   console.log(user.password)
+
+//     if (password !== user.password) {
+//       return res.status(401).json("Wrong Password");
+//     }
+
+//     const accessToken = jwt.sign(
+//       {
+//         id: user._id,
+//         isAdmin: user.isAdmin,
+//       },
+//       process.env.JWT_SEC,
+//       { expiresIn: "3d" }
+//     );
+
+//     const { password, ...others } = user._doc;
+//     return res.status(200).json({ ...others, accessToken });
+//   } catch (err) {
+//     console.log(err);
+//     return res.status(500).json(err);
+//   }
 });
+
+const generateToken = (id,isAdmin)=>{
+  return jwt.sign(
+        {
+          id,
+          isAdmin
+        },
+        process.env.JWT_SEC,
+        { expiresIn: "3d" }
+      );
+}
 
 module.exports = router;
